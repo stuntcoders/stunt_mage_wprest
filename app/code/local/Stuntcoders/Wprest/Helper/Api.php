586eb4471bdb299df7621207701ea1bb42a0a8fd
Mladen Ilic <mladen@stuntcoders.com>
2017-03-14 21:52:01 +0100

    Remove unused helper

--- app/code/local/Stuntcoders/Wprest/Helper/Api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Stuntcoders_Wprest_Helper_Api extends Mage_Core_Helper_Abstract
-{
-    public function getUrl($slug, array $query = array())
-    {
-        return Mage::getUrl($slug, array('_query' => $query));
-    }
-}
