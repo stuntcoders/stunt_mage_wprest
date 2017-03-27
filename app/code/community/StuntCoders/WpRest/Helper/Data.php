26a35de84ea069eac8594fbf50f8be65c2d31be5
Tom Scott-Malden <tom@skywire.co.uk>
2017-03-27 15:26:21 +0100

    Permalink
    - Added permalink helper
    - Updated model and config to use base url of wordpress without wp-json

--- app/code/community/StuntCoders/WpRest/Helper/Data.php
+++ app/code/community/StuntCoders/WpRest/Helper/Data.php
@@ -12,4 +12,16 @@ class StuntCoders_WpRest_Helper_Data extends Mage_Core_Helper_Abstract
     {
         return Mage::getStoreConfig(self::BASE_URI_CONFIG_PATH, $store);
     }
+
+    /**
+     * @param $link
+     *
+     * @return mixed
+     */
+    public function getPermaLink($link)
+    {
+        return Mage::getUrl(
+            str_replace($this->getBaseUri(), '', $link)
+        );
+    }
 }
