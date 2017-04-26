b8f423ba1f4851b85a69ce17af76017a20bf79ac
Tom Scott-Malden <tom@skywire.co.uk>
2017-04-26 15:20:27 +0100

    Capitolisation fix
    - Fixed casing of extended class names as casing sensitive systems would fail to find extended class

--- app/code/community/StuntCoders/WpRest/Model/Api/Post.php
+++ app/code/community/StuntCoders/WpRest/Model/Api/Post.php
@@ -1,6 +1,6 @@
 <?php
 
-class StuntCoders_WpRest_Model_Api_Post extends StuntCoders_Wprest_Model_Api_Abstract
+class StuntCoders_WpRest_Model_Api_Post extends StuntCoders_WpRest_Model_Api_Abstract
 {
     const ROUTE = '/wp/v2/posts/:id';
 
